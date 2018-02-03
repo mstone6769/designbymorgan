dd012efb6ae41a56d51f61329e008844ce266431
Morgan Stone <mstone@designbymorgan.com>
2018-02-03 13:13:06 -0600

    adding sass and reset

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,20 @@ module.exports = {
   siteMetadata: {
     title: 'Morgan Stone | UI/UX Achitect, Product Designer, and Front end Engineer',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-postcss-sass`,
+      options: {
+        postCssPlugins: [],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-1337770-1`,
+      },
+    },
+    `gatsby-plugin-offline`,
+  ],
 };
